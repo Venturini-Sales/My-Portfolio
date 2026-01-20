bade2ba1d2a9bfe64c32425ce7277c2fa2fc6657
João Pedro Venturini Sales <jpventurinisales@gmail.com>
2026-01-20 15:43:52 -0300

    Fix: Theme Import Fix

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { GlobalStyle } from "./Styles/GlobalStyles.jsx";
 import { ThemeProvider } from "styled-components";
-import { theme } from "./Styles/theme.jsx";
+import { theme } from "./Styles/Theme.jsx";
 import { CursorProvider } from "./Contexts/ContextCursor.jsx";
 
 createRoot(document.getElementById("root")).render(
@@ -14,5 +14,5 @@ createRoot(document.getElementById("root")).render(
         <App />
       </CursorProvider>
     </ThemeProvider>
-  </StrictMode>
+  </StrictMode>,
 );
